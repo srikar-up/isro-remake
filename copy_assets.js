2a4021af2b401b0c01e9d8d8877ec938812c230f
srikar-up <srikarsensai@gmail.com>
2026-08-13 00:44:36 +0530

    Fix Netlify build: untrack node_modules, add netlify.toml and SPA redirects

--- copy_assets.js
+++ copy_assets.js
@@ -1,14 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 
-const srcImg = path.join(process.cwd(), 'image files', 'eart png.png');
+const srcImg = path.join(process.cwd(), 'image files', 'earth.png');
 
 if (fs.existsSync(srcImg)) {
   fs.mkdirSync(path.join(process.cwd(), 'public'), { recursive: true });
   fs.mkdirSync(path.join(process.cwd(), 'src', 'assets'), { recursive: true });
   
   fs.copyFileSync(srcImg, path.join(process.cwd(), 'public', 'earth.png'));
-  fs.copyFileSync(srcImg, path.join(process.cwd(), 'public', 'eart png.png'));
   fs.copyFileSync(srcImg, path.join(process.cwd(), 'src', 'assets', 'earth.png'));
   fs.copyFileSync(srcImg, path.join(process.cwd(), 'src', 'earth.png'));
   
